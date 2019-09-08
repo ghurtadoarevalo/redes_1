3be01780d8a04529f1d5f493422d2225f5b3dc32
Gustavo Hurtado <gustavo.hurtado@usach.cl>
2019-09-08 00:28:25 -0300

    Se crea handel.wav después de la transformada inversa

--- lab1.py
+++ lab1.py
@@ -18,15 +18,15 @@ audio = data[1]
 #Como graficar archivos WAV a razon del tiempo
 #https://stackoverflow.com/questions/18625085/how-to-plot-a-wav-file
 
-tiempo = numpy.linspace(0,len(audio)/rate, num=len(audio))
+time = numpy.linspace(0,len(audio)/rate, num=len(audio))
 #matp.plot(tiempo, frecuencias)
 #matp.show()
 
-matp.plot(tiempo, audio)
+matp.plot(time, audio)
 
-matp.title('Gráfico amplitud vs tiempo')
-matp.ylabel('amplitud (db)')
-matp.xlabel('tiempo (s)')
+matp.title('Gráfico amplitud vs tiempo: Original')
+matp.ylabel('Amplitud ')
+matp.xlabel('Tiempo (s)')
 matp.show()
 
 #De acá obtuve cómo hacer el gráfico
@@ -34,18 +34,19 @@ matp.show()
 fourier = numpy.fft.fft(audio)
 fourierFreq = numpy.fft.fftfreq(audio.shape[-1])
 
-#fourierInv = numpy.fft.ifft(fourier)
-matp.plot(fourierFreq, fourier.real, fourierFreq, fourier.imag)
-matp.title('Gráfico amplitud vs frecuencia')
-matp.ylabel('amplitud (db)')
-matp.xlabel('frecuencia (hz)')
+matp.plot(fourierFreq, numpy.abs(fourier))
+matp.title('Gráfico amplitud vs frecuencia: Transformada de Fourier')
+matp.ylabel('Amplitud')
+matp.xlabel('Frecuencia (hz)')
 matp.show()
 
 
 fourierInv = numpy.fft.ifft(fourier)
 matp.plot(fourierFreq, fourierInv.real, fourierFreq, fourierInv.imag)
-matp.title('Gráfico amplitud vs tiempo')
-matp.ylabel('amplitud (db)')
-matp.xlabel('frecuencia (hz)')
+matp.title('Gráfico amplitud vs tiempo: Transformado')
+matp.ylabel('Amplitud')
+matp.xlabel('Frecuencia (hz)')
 matp.show()
+
+sci.write('handelTransformed.wav', rate, numpy.int16(fourierInv))
 #Buscar mas importantes
