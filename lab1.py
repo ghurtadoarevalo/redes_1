bef17fcc3c862420160715e3ca75b21a55af09a6
mcoronado <matias.coronado@usach.cl>
2019-09-13 20:15:38 -0300

    Comentarios agregados

--- lab1.py
+++ lab1.py
@@ -2,6 +2,10 @@ import scipy.io.wavfile as sci
 import matplotlib.pyplot as matp
 import numpy
 
+
+# Funcion: Conforma un grafico con los elementos de entrada, el cual muestra al final de proceso
+# Entradas: Datos del eje x e y, titulo del grafico, label's de ambos ejes.
+# Salida:   N/A
 def graph(datax, datay, title, ylabel, xlabel):
     matp.plot(datax, datay)
     matp.title(title)
@@ -9,43 +13,46 @@ def graph(datax, datay, title, ylabel, xlabel):
     matp.xlabel(xlabel)
     matp.show()
 
-#data compuesto por (rate, arreglo(hz0,hz1,...,hzn),dtype)
 
-#Rate relacionado con velocidad de reproduccion?
-#Arreglo de frecuencias medidas en hz
-#dtype: Indica el tipo de formato WAV que se esta recibiendo.
-#		Por cada formato se tiene una frecuencia maxima y minima
+# -------------------------------------------------------- PUNTO 1 --------------------------------------------------------
+
+# Se lee el archivo de audio de entrada con la funcion read de scipy.
+# el cual entrega el valor del rate, ademas de un arreglo de las respectivas amplitudes del audio junto con su dtype.
 data = sci.read('handel.wav')
 rate = data[0]
 audio = data[1]
 
-#Como graficar archivos WAV a razon del tiempo
-#https://stackoverflow.com/questions/18625085/how-to-plot-a-wav-file
+# -------------------------------------------------------- PUNTO 2 --------------------------------------------------------
 
+# A razon de los valores obtenidos anteriormente, se conforma un arreglo de tiempos con ayuda de la funcion
+# linspace de numpy, el cual toma como entrada: (valor del primer elemento, valor del ultimo elemento, total de elementos)
 time = numpy.linspace(0,len(audio)/rate, num=len(audio))
-#matp.plot(tiempo, frecuencias)
-#matp.show()
 
+# Se grafica las amplitudes del audio en funcion del tiempo
 graph(time,audio,"Gráfico amplitud vs tiempo: Original", "Amplitud", "Tiempo (s)")
 
-#De acá obtuve cómo hacer el gráfico
-#https://docs.scipy.org/doc/numpy/reference/generated/numpy.fft.fft.html
+# -------------------------------------------------------- PUNTO 3 --------------------------------------------------------
+
+# Se calcula la tranformada de fourier a partir de las amplitudes del audio, por medio de la funcion fft de numpy.
 fourier = numpy.fft.fft(audio)
-
-#De acá obtuve cómo poner la frecuencia correcta
-#https://pythondsp.rob-elder.com/loading-wav-files-and-showing-frequency-response/
-fourierFreq = numpy.fft.fftfreq(audio.shape[0], 1/rate)
-
-graph(fourierFreq,numpy.abs(fourier),"Gráfico amplitud vs frecuencia: Transformada de Fourier", "Amplitud", "Frecuencia (Hz)")
-
+# --DUDA--
+# Se utiliza la funcion fftfreq para obtener un arreglo compuesto por el rango de frecuencias del audio.
+fourierFreq = numpy.fft.fftfreq(len(audio), 1/rate)
+# Se obtiene la transformada de fourier inversa de lo obtenido en el punto anterior. Esto por medio de la funcion ifft de numpy.
 fourierInv = numpy.fft.ifft(fourier)
 
+# Se grafica en el dominio de la frecuencia, utilizando los valores absolutos de fourier
+graph(fourierFreq,numpy.abs(fourier),"Gráfico amplitud vs frecuencia: Transformada de Fourier", "Amplitud", "Frecuencia (Hz)")
+# Se grafica las transformada de fourier inversa a razon del tiempo.
 graph(time, fourierInv,"Gráfico amplitud vs tiempo: Transformado", "Amplitud", "Tiempo (s)")
 
-sci.write('handelTransformed.wav', rate, numpy.int16(fourierInv))
+#sci.write('handelTransformed.wav', rate, numpy.int16(fourierInv))
 
+
+# -------------------------------------------------------- PUNTO 4 --------------------------------------------------------
+# Se trunca a 0 los valores que se encuentren fuera de de los rangos de frecuencia principales, los cuales se obtuvieron
+# a partir de un previo analisis visual.
 fourierTrunc = fourier
-
 for i in range(11156, len(fourierTrunc)):
     if fourierFreq[i] < 0:
         for j in range(i, 61957):
@@ -54,11 +61,13 @@ for i in range(11156, len(fourierTrunc)):
     else:
         fourierTrunc[i] = 0
 
-
+# Se calcula la transformada de fourier a lo trucado anteriormente.
 fourierTruncInv = numpy.fft.ifft(fourierTrunc)
 
+# Se grafica la transformada de fourier inversa a razon del tiempo.
 graph(time, fourierTruncInv,"Gráfico amplitud vs tiempo: Truncado", "Amplitud", "Tiempo (s)")
 
+# Se grafica en el dominio de la frecuencia, utilizando los valores absolutos del fourier truncado
 graph(fourierFreq, numpy.abs(fourierTrunc),"Gráfico amplitud vs frecuencia: Truncado", "Amplitud", "Frecuencia (hz)")
 
-sci.write('handelTransformedTrunc.wav', rate, numpy.int16(fourierTruncInv))
+#sci.write('handelTransformedTrunc.wav', rate, numpy.int16(fourierTruncInv))
